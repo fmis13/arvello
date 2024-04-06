487e5dd717a2cf7bcf06ec5828779ebf1bdfe68d
fmis13 <76878393+fmis13@users.noreply.github.com>
2024-04-06 10:01:30 +0200

    add functionality for adding multiple products and their quantities, prep for rabat and discount. Fixes.

--- arvello/arvelloapp/forms.py
+++ arvello/arvelloapp/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import *
 from django.core.exceptions import ValidationError
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Column, Div, HTML, Layout, Row
+from django.forms import BaseInlineFormSet, inlineformset_factory, ModelForm
+from django.forms.widgets import NumberInput
 
 class DateInput(forms.DateInput):
     input_type = 'date'
@@ -32,36 +36,137 @@ class ClientForm(forms.ModelForm):
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['title', 'description', 'quantity', 'price', 'taxPercent', 'currency']
+        fields = ['title', 'description', 'price', 'taxPercent', 'currency']
         labels = {
             'title': 'Naziv proizvoda', 'description': 'Opis proizvoda',
-            'quantity': 'Količina', 'price': 'Cijena', 'currency': 'Valuta', 'taxPercent': 'Porez (%)',
+            'price': 'Cijena', 'currency': 'Valuta', 'taxPercent': 'Porez (%)',
         }
 
 
 class InvoiceForm(forms.ModelForm):
     dueDate = forms.DateField(required = True, label='Datum dospijeća', widget=DateInput(attrs={'class': 'form-control'}),)
     date = forms.DateField(required = True, label='Datum računa', widget=DateInput(attrs={'class': 'form-control'}),)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Row(
+                Column('title', css_class='form-group col-lg-3 col-md-3 col-sm-6 mb-0'),   
+                Column('number', css_class='form-group col-lg-3 col-md-3 col-sm-6 mb-0'),
+                Column('date', css_class='form-group col-lg-3 col-md-3 col-sm-3 mb-0'),
+                Column('dueDate', css_class='form-group col-lg-3 col-md-3 col-sm-3 mb-0'),
+                Column('client', css_class='form-group col-lg-3 col-md-3 col-sm-6 mb-0'),
+                Column('subject', css_class='form-group col-lg-3 col-md-3 col-sm-6 mb-0'),
+                Column('notes', css_class='form-group col-lg-3 col-md-3 col-sm-6 mb-0'),
+                css_class='form-row'
+            )
+        )
     class Meta:
         model = Invoice
-        fields = ['title', 'number', 'dueDate', 'notes', 'client', 'product', 'date', 'subject']
+        fields = ['title', 'number', 'dueDate', 'notes', 'client', 'date', 'subject']
         labels = {
             'title': 'Naslov', 'number': 'Broj računa',
             'dueDate': 'Datum dospijeća', 'date': 'Datum računa', 'notes': 'Napomene',
             'client': 'Klijent', 'product': 'Proizvod', 'subject': 'Subjekt'
         }
 
+class InvoiceProductForm(ModelForm):
+    product = forms.ModelChoiceField(queryset=Product.objects.order_by('title'))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+
+            Row(
+                Column('id', type="hidden", css_class="d-none"),
+                Column('DELETE', type="hidden", css_class="d-none"),
+                Column('product', css_class='form-group col-lg-3 col-md-3 col-sm-5 mb-0'),
+                Column('quantity', css_class='form-group col-lg-3 col-md-3 col-sm-3 mb-0'),
+                Column('rabat', css_class='form-group col-lg-3 col-md-3 col-sm-3 mb-0'),
+                Column('discount', css_class='form-group col-lg-3 col-md-3 col-sm-3 mb-0'),
+                Div(HTML("""<label class='empty-div form-label'>&nbsp</label>
+                            <button type="button" class="buttonDynamic btn btn-primary align-middle {% if forloop.first %}first-button{% endif %}">+</button>"""),
+                    css_class="form-group col-lg-1 col-md-1 col-sm-1 mb-0 box-btn-add-product"), css_class="formsetDynamic")
+        )
+
+    class Meta:
+        model = InvoiceProduct
+        fields = ['product', 'quantity', 'rabat', 'discount']
+
+class BaseInlineInvoiceProductSet(BaseInlineFormSet):
+    deletion_widget = forms.HiddenInput
+
+
+InvoiceProductFormSet = inlineformset_factory(
+    Invoice, InvoiceProduct, form=InvoiceProductForm, formset=BaseInlineInvoiceProductSet, fields=('product', 'quantity', 'rabat', 'discount'), extra=1, can_delete=True)
+
+
 class OfferForm(forms.ModelForm):
     dueDate = forms.DateField(required = True, label='Datum dospijeća', widget=DateInput(attrs={'class': 'form-control'}),)
     date = forms.DateField(required = True, label='Datum ponude', widget=DateInput(attrs={'class': 'form-control'}),)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Row(
+                Column('title', css_class='form-group col-lg-3 col-md-3 col-sm-6 mb-0'),   
+                Column('number', css_class='form-group col-lg-3 col-md-3 col-sm-6 mb-0'),
+                Column('date', css_class='form-group col-lg-3 col-md-3 col-sm-3 mb-0'),
+                Column('dueDate', css_class='form-group col-lg-3 col-md-3 col-sm-3 mb-0'),
+                Column('client', css_class='form-group col-lg-3 col-md-3 col-sm-6 mb-0'),
+                Column('subject', css_class='form-group col-lg-3 col-md-3 col-sm-6 mb-0'),
+                Column('notes', css_class='form-group col-lg-3 col-md-3 col-sm-6 mb-0'),
+                css_class='form-row'
+            )
+        )
     class Meta:
         model = Offer
-        fields = ['title', 'number', 'dueDate', 'notes', 'client', 'product', 'date', 'subject']
+        fields = ['title', 'number', 'dueDate', 'notes', 'client', 'date', 'subject']
         labels = {
             'title': 'Naslov', 'number': 'Broj računa',
             'dueDate': 'Datum dospijeća', 'notes': 'Napomene',
             'client': 'Klijent', 'product': 'Proizvod', 'date': 'Datum ponude', 'subject': 'Subjekt'
         }
+
+class OfferProductForm(ModelForm):
+    product = forms.ModelChoiceField(queryset=Product.objects.order_by('title'))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+
+            Row(
+                Column('id', type="hidden", css_class="d-none"),
+                Column('DELETE', type="hidden", css_class="d-none"),
+                Column('product', css_class='form-group col-lg-3 col-md-3 col-sm-5 mb-0'),
+                Column('quantity', css_class='form-group col-lg-3 col-md-3 col-sm-3 mb-0'),
+                Column('rabat', css_class='form-group col-lg-3 col-md-3 col-sm-3 mb-0'),
+                Column('discount', css_class='form-group col-lg-3 col-md-3 col-sm-3 mb-0'),
+                Div(HTML("""<label class='empty-div form-label'>&nbsp</label>
+                            <button type="button" class="buttonDynamic btn btn-primary align-middle {% if forloop.first %}first-button{% endif %}">+</button>"""),
+                    css_class="form-group col-lg-1 col-md-1 col-sm-1 mb-0 box-btn-add-product"), css_class="formsetDynamic")
+        )
+
+    class Meta:
+        model = OfferProduct
+        fields = ['product', 'quantity', 'rabat', 'discount']
+        labels = {
+            'product': 'Proizvod', 'quantity': 'Količina', 'rabat': 'Rabat', 'discount': 'Popust'
+        }
+
+class BaseInlineOfferProductSet(BaseInlineFormSet):
+    deletion_widget = forms.HiddenInput
+
+
+OfferProductFormSet = inlineformset_factory(
+    Offer, OfferProduct, form=OfferProductForm, formset=BaseInlineOfferProductSet, fields=('product', 'quantity', 'rabat', 'discount'), extra=1, can_delete=True)
+
 
 class CompanyForm(forms.ModelForm):
     class Meta:
--- arvello/arvelloapp/views.py
+++ arvello/arvelloapp/views.py
@@ -9,6 +9,9 @@ from django.contrib.auth.models import User, auth
 from io import BytesIO
 from .forms import *
 from .models import *
+import requests
+import json
+import base64
 
 def anonymous_required(function=None, redirect_url=None):
 
@@ -78,6 +81,86 @@ def invoices(request):
         form = InvoiceForm()
 
     return render(request, 'invoices.html', {'form': form}, context)
+
+@login_required
+def create_invoice(request):
+    if request.method == 'POST':
+        invoice_form = InvoiceForm(request.POST)
+        invoice_formset = InvoiceProductFormSet(request.POST)
+
+        if invoice_form.is_valid() and invoice_formset.is_valid():
+            invoice = invoice_form.save()
+            invoice_products = []
+            total = 0
+            for invoice_form in invoice_formset:
+                invoice_product = invoice_form.save(commit=False)
+                invoice_product.invoice = invoice
+                invoice_product.product = invoice_form.cleaned_data["product"]
+                total += Decimal(invoice_product.product.price) * invoice_product.quantity
+                invoice_products.append(invoice_product)
+
+            invoice.total = total
+            invoice.save()
+
+            for invoice_product in invoice_products:
+                invoice_product.save()
+            messages.success(request, 'Nadodan je novi račun')
+            return redirect('invoices')
+        if not invoice_formset.is_valid():
+            print(invoice_formset.errors)
+            messages.error(request, 'Problem pri obradi zahtjeva')
+    else:
+        invoice_form = InvoiceForm()
+        invoice_formset = InvoiceProductFormSet(queryset=InvoiceProduct.objects.none())
+
+    empty_form = InvoiceProductFormSet(queryset=InvoiceProduct.objects.none())
+    context = {
+        'invoice_form': invoice_form,
+        'invoice_formset': invoice_formset,
+        'empty_form': empty_form,
+    }
+
+    return render(request, 'makeinvoice.html', context)
+
+@login_required
+def create_offer(request):
+    if request.method == 'POST':
+        offer_form = OfferForm(request.POST)
+        offer_formset = OfferProductFormSet(request.POST)
+
+        if offer_form.is_valid() and offer_formset.is_valid():
+            offer = offer_form.save()
+            offer_products = []
+            total = 0
+            for offer_form in offer_formset:
+                offer_product = offer_form.save(commit=False)
+                offer_product.offer = offer
+                offer_product.product = offer_form.cleaned_data["product"]
+                total += Decimal(offer_product.product.price) * offer_product.quantity
+                offer_products.append(offer_product)
+
+            offer.total = total
+            offer.save()
+
+            for offer_product in offer_products:
+                offer_product.save()
+            messages.success(request, 'Nadodana je nova ponuda')
+            return redirect('offers')
+        if not offer_formset.is_valid():
+            print(offer_formset.errors)
+            messages.error(request, 'Problem pri obradi zahtjeva')
+    else:
+        offer_form = OfferForm()
+        offer_formset = OfferProductFormSet(queryset=OfferProduct.objects.none())
+
+    empty_form = OfferProductFormSet(queryset=OfferProduct.objects.none())
+    context = {
+        'offer_form': offer_form,
+        'offer_formset': offer_formset,
+        'empty_form': empty_form,
+    }
+
+    return render(request, 'makeoffer.html', context)
 
 @login_required
 def companies(request):
@@ -192,16 +275,87 @@ def login(request):
 def invoice_pdf(request, pk):
     invoice = get_object_or_404(Invoice, pk=pk)
     subject = invoice.subject
-    product = invoice.product.all()
+    product = InvoiceProduct.objects.filter(invoice=invoice)
     client = invoice.client
-    return render(request, 'invoice_export_view.html', {'invoice': invoice, 'products': product, 'client': client, 'subject': subject})
+    url = "https://hub3.bigfish.software/api/v2/barcode"
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        "renderer": "image",
+        "options": {
+            "format": "png",
+            "color": "#000000",
+            "bgColor": "#ffffff",
+            "scale": 3,
+            "ratio": 3
+        },
+        "data": {
+            "amount": int(invoice.total100()),
+            "currency": invoice.currtext(),
+            "sender": {
+                "name": client.clientName,
+                "street": client.addressLine1,
+                "place": client.postalCode + " " + client.province,
+            },
+            "receiver": {
+                "name": subject.clientName,
+                "street": subject.addressLine1,
+                "place": subject.postalCode + " " + subject.province,
+                "iban": subject.IBAN,
+                "model": "00",
+                "reference": invoice.reference(),
+            },
+            "purpose": "",
+            "description": "Uplata po računu " + invoice.number,
+        }
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    barcode_image = base64.b64encode(response.content).decode()
+    print(response.status_code)
+    print(response.content)
+    return render(request, 'invoice_export_view.html', {'invoice': invoice, 'products': product, 'client': client, 'subject': subject, 'barcode_image': barcode_image})
 
+@login_required
 def offer_pdf(request, pk):
     offer = get_object_or_404(Offer, pk=pk)
     subject = offer.subject
-    product = offer.product.all()
+    product = OfferProduct.objects.filter(offer=offer)
     client = offer.client
-    return render(request, 'offer_export_view.html', {'offer': offer, 'products': product, 'client': client, 'subject': subject})
+    url = "https://hub3.bigfish.software/api/v2/barcode"
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        "renderer": "image",
+        "options": {
+            "format": "png",
+            "color": "#000000",
+            "bgColor": "#ffffff",
+            "scale": 3,
+            "ratio": 3
+        },
+        "data": {
+            "amount": int(offer.total100()),
+            "currency": offer.currtext(),
+            "sender": {
+                "name": client.clientName,
+                "street": client.addressLine1,
+                "place": client.postalCode + " " + client.province,
+            },
+            "receiver": {
+                "name": subject.clientName,
+                "street": subject.addressLine1,
+                "place": subject.postalCode + " " + subject.province,
+                "iban": subject.IBAN,
+                "model": "00",
+                "reference": offer.reference(),
+            },
+            "purpose": "",
+            "description": "Uplata po računu " + offer.number,
+        }
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    barcode_image = base64.b64encode(response.content).decode()
+    print(response.status_code)
+    print(response.content)
+    return render(request, 'offer_export_view.html', {'offer': offer, 'products': product, 'client': client, 'subject': subject, 'barcode_image': barcode_image})
 
 @login_required
 def logout(request):
